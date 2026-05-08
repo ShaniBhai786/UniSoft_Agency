b042159c0d5f811f57db162ba00ea7b9fe9d0885
Roshaan <roshaanahmad91@gmail.com>
2026-05-08 17:13:59 +0500

    fix: adjust background gradient opacity for improved overlay effect

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react"
+import React from "react"
 import "../styles.css"
 import { motion } from "framer-motion"
 import { TypeAnimation } from 'react-type-animation'
