3b45838cddf5a45dd8af7e587a1424c7ce0dd1d0
Roshaan <roshaanahmad91@gmail.com>
2026-06-06 18:18:58 +0500

    feat: add quote styling elements to blog detail page

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -64,6 +64,7 @@ export default async function BlogDetail({ params }) {
                     }),
                 }}
             />
+
             <div className="quoteGradient" />
             <div className="quoteGrid" />
 
