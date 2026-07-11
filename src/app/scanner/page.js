814a7a9bfcf2ee57cc495b7c96c3ef60823849d2
Roshaan <roshaanahmad91@gmail.com>
2026-07-11 22:17:49 +0500

    Add Scanner page layout and styles for attendance marking

--- src/app/scanner/page.js
+++ src/app/scanner/page.js
@@ -1,10 +1,15 @@
 import Scanner from "../components/Scanner";
+import "../scanner.css";
 
 export default function Page() {
     return (
-        <div>
-            <h1>Attendance Scanner</h1>
-            <Scanner />
+        <div className="scannerPage">
+            <div className="scannerCard">
+                <h1>Attendance Scanner</h1>
+                <p>Scan Student Barcode to Mark Attendance</p>
+
+                <Scanner />
+            </div>
         </div>
     );
 }
